4b6fb2a99de457f583b9558d45255a83dda5fcd0
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-08 12:29:43 +0200

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage';
 
