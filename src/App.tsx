1329915504c54e90ecfb9b71491a2a9df7f720a2
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-11 10:23:08 +0200

    Add routes for login, repairs and welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Route, useHistory, useLocation } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import RepairsPage from './pages/RepairsPage';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const location = useLocation();
+  const history = useHistory();
+  const authContext = useContext(AuthContext);
+
+  if (location.pathname === '/' && authContext.isLoggedIn) {
+    history.push('/repairs');
+  }
+
+  if (location.pathname !== '/' && location.pathname !== '/login' && !authContext.isLoggedIn) {
+    history.push('/login');
+  }
+
   return (
     <Layout>
       <Route path='/' exact>
         <HomePage/>
       </Route>
-      <Route path='/login'>
-        <p>login</p>
-      </Route>
+      { !authContext.isLoggedIn && <Route path='/login'>
+        <AuthPage />
+      </Route>}
+      { authContext.isLoggedIn && <Route path='/repairs'>
+        <RepairsPage />
+      </Route>}
     </Layout>
   );
 }
