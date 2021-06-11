aaa1165d2b63a860d9a54ab825041fa6fb58d109
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-11 10:23:40 +0200

    Add login, repairs and logout navigation items

--- src/components/Layout/MainNavigation.tsx
+++ src/components/Layout/MainNavigation.tsx
@@ -1,15 +1,25 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const authContext = useContext(AuthContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Auto shop repair</div>
       <nav className={classes.nav}>
         <ul>
-          <li>
+          {!authContext.isLoggedIn && <li>
             <NavLink to='/login' activeClassName={classes.active}>Login</NavLink>
-          </li>
+          </li>}
+          {authContext.isLoggedIn && <li>
+            <NavLink to='/repairs' activeClassName={classes.active}>Repairs</NavLink>
+          </li>}
+          {authContext.isLoggedIn && <li>
+            <NavLink to='/' activeClassName={classes.active} onClick={authContext.logout} exact>Logout</NavLink>
+          </li>}
         </ul>
       </nav>
     </header>
