3564f217010426053ebbe698296a646bec2c17a5
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-14 10:38:02 +0200

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
-import { Route, useHistory, useLocation } from 'react-router-dom';
+import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import AuthPage from './pages/AuthPage';
+import EditUserPage from './pages/EditUserPage';
 import HomePage from './pages/HomePage';
 import NewUserPage from './pages/NewUserPage';
+import NotFoundPage from './pages/NotFoundPage';
 import RepairsPage from './pages/RepairsPage';
 import UsersPage from './pages/UsersPage';
 import AuthContext from './store/auth-context';
@@ -23,21 +25,29 @@ function App() {
 
   return (
     <Layout>
-      <Route path='/' exact>
-        <HomePage/>
-      </Route>
-      { !authContext.isLoggedIn && <Route path='/login'>
-        <AuthPage />
-      </Route>}
-      { authContext.isLoggedIn && <Route path='/repairs'>
-        <RepairsPage />
-      </Route>}
-      { authContext.isLoggedIn && authContext.role === 'manager' && <Route path='/users'>
-        <UsersPage />
-      </Route>}
-      { authContext.isLoggedIn && authContext.role === 'manager' && <Route path='/new-user'>
-        <NewUserPage />
-      </Route>}
+      <Switch>
+        <Route path='/' exact>
+          <HomePage/>
+        </Route>
+        { !authContext.isLoggedIn && <Route path='/login'>
+          <AuthPage />
+        </Route>}
+        { authContext.isLoggedIn && <Route path='/repairs'>
+          <RepairsPage />
+        </Route>}
+        { authContext.isLoggedIn && authContext.role === 'manager' && <Route path='/users/:id'>
+          <EditUserPage />
+        </Route>}
+        { authContext.isLoggedIn && authContext.role === 'manager' && <Route path='/users'>
+          <UsersPage />
+        </Route>}
+        { authContext.isLoggedIn && authContext.role === 'manager' && <Route path='/new-user'>
+          <NewUserPage />
+        </Route>}
+        <Route path='*'>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </Layout>
   );
 }
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className='centered'>
+      <p>Page not found!</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
