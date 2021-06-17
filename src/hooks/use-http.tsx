7f28db16745ade9315125ad81d074855818815dc
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-17 16:16:43 +0200

    useHttp custom hook

---
+++ src/hooks/use-http.tsx
@@ -0,0 +1,72 @@
+import { useReducer, useCallback } from "react";
+import RequestData from "../models/Request";
+
+type HttpState<T> = {
+  data: T | null
+  error: string | null,
+  isLoading: boolean,
+  status: string
+}
+
+type HttpAction<T> =  { type: 'pending' } | { type: 'success'; payload: { data: T } } | { type: 'fail'; payload: { error: string} };
+
+const initialState = {
+  data: null,
+  error: null,
+  status: '',
+  isLoading: false,
+}
+
+const createHttpReducer = <T,>() => (state: HttpState<T>, action: HttpAction<T>) => {
+  if (action.type === 'pending') {
+    return {
+      ...state,
+      isLoading: true,
+      error: null,
+      status: 'pending'
+    }
+  }
+  if (action.type === 'success') {
+    return {
+      ...state,
+      isLoading: false,
+      error: null,
+      data: action.payload.data,
+      status: 'completed'
+    }
+  }
+  if (action.type === 'fail') {
+    return {
+      ...state,
+      isLoading: false,
+      error: action.payload.error,
+      data: null,
+      status: 'completed'
+    }
+  }
+  return initialState;
+}
+
+
+const useHttp = <T,> (requestFunction: (requestData: RequestData) => Promise<T>) => {
+  const httpReducer = createHttpReducer<T>();
+  const [httpState, dispatch] = useReducer(httpReducer, initialState);
+  
+  const sendRequest = useCallback(async(requestData: RequestData) => {
+    try {
+      dispatch({ type: 'pending'});
+      const data = await requestFunction(requestData);
+      dispatch({ type: 'success', payload: { data } });
+    } catch (error) {
+      dispatch({ type: 'fail', payload: { error }});
+    }
+  }, [ requestFunction ]);
+
+  return {
+    ...httpState,
+    sendRequest
+  }
+
+}
+
+export default useHttp;
