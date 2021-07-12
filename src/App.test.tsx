3d02cf4dc696c4667a25c5dc64dfcaa125eeecc3
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-07-12 17:25:46 +0200

    App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter, MemoryRouter, Router, useHistory } from 'react-router-dom';
+import { AuthProvider } from './store/auth-context';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Welcome page', () => {
+  beforeEach(() => {
+    render(
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter> 
+      </AuthProvider>
+    );
+  });
+
+  test('renders welcome page', () => {
+    const welcomeHeading = screen.getByText(/Welcome to auto show repair/i);
+    expect(welcomeHeading).toBeInTheDocument();
+  });
+
+  test('renders login link', () => {
+    const linkElement = screen.getByText(/login/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+});
+
+
+describe('Routes', () => {
+  test('renders <AuthForm /> component for "/login" route', () => {
+    render(
+      <AuthProvider>
+        <MemoryRouter initialEntries={["/login"]}>
+          <App />
+        </MemoryRouter> 
+      </AuthProvider>
+    );
+    const signInHeading = screen.getByText(/Sign In/i);
+    expect(signInHeading).toBeInTheDocument();
+  });
 });
