d4d99d0ccd219b8cd3235958846ff8fb152e0954
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-07-13 16:20:38 +0200

    RepairForm test

---
+++ src/components/Repairs/RepairForm.test.tsx
@@ -0,0 +1,45 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import Repair from '../../models/Repair';
+import RepairForm from './RepairForm';
+
+describe('Repair Form', () => {
+
+  const users = [
+    {
+      id: '1',
+      fullName: 'Learner One',
+      role: 'user'
+    },
+    {
+      id: '2',
+      fullName: 'Learner Two',
+      role: 'user'
+    }
+  ]
+
+  beforeEach(() => {
+    render(<RepairForm onSubmit={(repair: Repair) => {}} users={users}/>);
+  });
+
+  test('renders Repair form', () => {
+    const repairFormElement = screen.getByText(/Repair Form/i);
+    expect(repairFormElement).toBeInTheDocument();
+  });
+
+  test('renders description error if description field is empty', () => {
+    const descriptionTextArea = screen.getByLabelText('description-textarea');
+    fireEvent.change(descriptionTextArea, { target: { value: ''}});
+    fireEvent.focusOut(descriptionTextArea);
+    const error = screen.queryByText('Please enter a description.');
+    expect(error).toBeInTheDocument();
+  });
+
+  test('renders date error if date field is empty', () => {
+    const dateInput = screen.getByLabelText('date-input');
+    fireEvent.change(dateInput, { target: { value: ''}});
+    fireEvent.focusOut(dateInput);
+    const error = screen.queryByText('Please enter a date.');
+    expect(error).toBeInTheDocument();
+  });
+
+});
